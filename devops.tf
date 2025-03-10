c441209eb185ce926d393f03cb75b0b21684c791
bunny3107 <vamshiyathakula3@gmail.com>
2025-03-10 17:58:33 +0530

    Create devops.tf

---
+++ devops.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+region = "us-east-1"
+}
+resource "aws_instance" "web" {
+  ami           = "ami-08b5b3a93ed654d19"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "devops"
+  }
+}
